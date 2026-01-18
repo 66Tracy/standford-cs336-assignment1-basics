cd6f1d7b39f2b46251ceb40d8b22fe93467636a5
66Tracy <244090087@qq.com>
2026-01-18 20:25:40 +0800

    version-1

--- cs336_basics/test.py
+++ cs336_basics/test.py
@@ -1,5 +1,37 @@
-from typing import List
+from typing import List, ByteString
 from functools import cmp_to_key
+
+class BytePair:
+    """字节对类型"""
+    left: ByteString
+    right: ByteString
+
+    def __init__(self, left: ByteString, right: ByteString):
+        self.left = left
+        self.right = right
+    
+    def __getitem__(self, index: int) -> ByteString:
+        """支持索引访问"""
+        if index == 0:
+            return self.left
+        elif index == 1:
+            return self.right
+        elif isinstance(index, slice):
+            # 支持切片
+            return (self.left, self.right)[index]
+        else:
+            raise IndexError(f"Index {index} out of range for BytePair")
+    
+    def __len__(self) -> int:
+        """支持 len() 函数"""
+        return 2
+    
+    def __repr__(self):
+        return f"BytePair({self.left!r}, {self.right!r})"
+    
+    def __str__(self):
+        return f"({self.left},{self.right})"
+
 
 def merge_bytepair_to_source_word(word_tokening:list):
     """从一个分词状态，反推出原单词"""
@@ -11,6 +43,26 @@ def merge_bytepair_to_source_word(word_tokening:list):
 
 
 if __name__ == "__main__":
-    candidate_list = [("A".encode("utf-8"), "B".encode("utf-8")), ("B".encode("utf-8"), "C".encode("utf-8")), ("C".encode("utf-8"), "ZZ".encode("utf-8")), ("ZZ".encode("utf-8"), "A".encode("utf-8"))]
-    print(merge_bytepair_to_source_word(candidate_list))
+    # candidate_list = [("A".encode("utf-8"), "B".encode("utf-8")), ("B".encode("utf-8"), "C".encode("utf-8")), ("C".encode("utf-8"), "ZZ".encode("utf-8")), ("ZZ".encode("utf-8"), "A".encode("utf-8"))]
+    # print(merge_bytepair_to_source_word(candidate_list))
     # “A”, “B”), (“A”, “C”), (“B”, “ZZ”),and (“BA”, “A”)
+
+    # word = "widest"
+    # word_encoding = word.encode("utf-8")
+    # word_split = [word_encoding[i:i+1] for i in range(len(word_encoding))]
+
+    # word_tokenizing = []
+    # for i in range(len(word_split)-1):
+    #     print(word_split[i], type(word_split[i]))
+    #     byte_pair = BytePair(word_split[i], word_split[i+1])
+    #     word_tokenizing.append(byte_pair)
+    
+    # print(word_tokenizing)
+
+    # 按字符拆分而不是按字节拆分
+    word = "café"
+        # 直接按字符拆分
+    word_split = [ch.encode("utf-8") for ch in word]
+    print(word_split)
+        # 或者如果你需要字符本身而不是编码
+        # word_split = list(word)  # ['c', 'a', 'f', 'é']
--- cs336_basics/training_startups.py
+++ cs336_basics/training_startups.py
@@ -1,10 +1,10 @@
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, ByteString
 from pretokenization_example import find_chunk_boundaries
 import regex as re
 from collections import defaultdict
 from functools import cmp_to_key
 import json
-from time import sleep
+import time
 
 class BPETrainer:
     def __init__(self, input_path: str, vocab_size: int, special_tokens: List[str]):
@@ -21,7 +21,7 @@ class BPETrainer:
         self.BPE_TOKEN_TABLE = {}    
         for i in range(256):
             self.BPE_TOKEN_TABLE[i] = bytes([i])
-        print("#########【测试点位-1】：初始化词表\n", self.BPE_TOKEN_TABLE, "\n\n\n")
+        # print("#########【测试点位-1】：初始化词表\n", self.BPE_TOKEN_TABLE, "\n\n\n")
 
         ## Step-3: 迭代训练
         self.train_bpe()
@@ -62,9 +62,9 @@ class BPETrainer:
 
     def merge_bytepair_to_source_word(self, word_tokenizing:list):
         """从一个分词状态，反推出原单词"""
-        source_word = bytes(word_tokenizing[0][0]).decode("utf-8") + bytes(word_tokenizing[0][1]).decode("utf-8")
+        source_word = word_tokenizing[0][0].decode("utf-8") + word_tokenizing[0][1].decode("utf-8")
         for i in range(1,len(word_tokenizing)):
-            source_word += bytes(word_tokenizing[i][1]).decode("utf-8")
+            source_word += word_tokenizing[i][1].decode("utf-8")
         
         return source_word
 
@@ -72,15 +72,22 @@ class BPETrainer:
     def merge_and_update_bytepair(self, most_frequent_pair: Tuple[bytes, bytes]):
         """根据传入的bytepair"""
         # 先对自身做更新
-        new_token = bytes([most_frequent_pair[0]]) + bytes([most_frequent_pair[1]])
+        new_token = most_frequent_pair[0] + most_frequent_pair[1]
+        # print(f"#########【测试点位-6】：新token：{new_token}\n\n\n")
 
         # 源词的序列列表
         source_word_index_list = self.bytepair_from_words_index[most_frequent_pair]
-        new_source_word_index_list = []
-        for index in source_word_index_list:
-            i, j = index[0], index[1]
+        # print(f"#########【测试点位-7】：源词的序列列表：{source_word_index_list}\n\n\n")
+        for pos, index in enumerate(source_word_index_list):
             # 获取源词的分词状态
-            word_tokenizing = self.words_tokenizing_states[i]
+            word_tokenizing = self.words_tokenizing_states[index]
+            # 反查i,j
+            j = 0
+            while j < len(word_tokenizing):
+                if most_frequent_pair == word_tokenizing[j]:
+                    break
+                j += 1
+            # print(f"#########【测试点位-8】：源词的分词状态：{word_tokenizing}：{index}\n\n\n")
             # 需要合并词元，反推出当前词的个数
             source_word = self.merge_bytepair_to_source_word(word_tokenizing)
             source_word_count = self.words_counting[source_word]
@@ -88,45 +95,47 @@ class BPETrainer:
             if j >= 1:
                 left_pair = word_tokenizing[j-1]
                 self.bytepair_counting_dict[left_pair] -= source_word_count
-                new_left_pair = (bytes([left_pair[0]]), new_token)
+                new_left_pair = (left_pair[0], new_token)
                 self.bytepair_counting_dict[new_left_pair] += source_word_count
-                # 修改左边的pair的倒查索引
-                left_pair_source_word_index_list = self.bytepair_from_words_index[left_pair]
-                target_idx = 0
-                while target_idx < len(left_pair_source_word_index_list):
-                    if left_pair_source_word_index_list[target_idx][0] == i and left_pair_source_word_index_list[target_idx][1] == j-1:
+                # 反查列表，new_left_pair要新增，left_pair要删减
+                self.bytepair_from_words_index[new_left_pair].append(index)
+                # 更新word_tokenizing状态，left_pair -> new_left_pair
+                word_tokenizing[j-1] = new_left_pair
+                del_idx = 0
+                while del_idx < len(self.bytepair_from_words_index[left_pair]):
+                    if self.bytepair_from_words_index[left_pair][del_idx] == index:
                         break
-                    target_idx += 1
-                left_pair_source_word_index_list.pop(target_idx)
-                self.bytepair_from_words_index[left_pair] = left_pair_source_word_index_list
+                    del_idx += 1
+                self.bytepair_from_words_index[left_pair].pop(del_idx)
+                # print(f"#########【测试点位-9】：左边新pair和更新后的统计 {new_left_pair}\n{self.bytepair_counting_dict}\n\n\n")
 
             if j <= len(word_tokenizing)-2:
                 right_pair = word_tokenizing[j+1]
                 self.bytepair_counting_dict[right_pair] -= source_word_count
-                new_right_pair = (new_token, bytes([right_pair[1]]))
+                new_right_pair = (new_token, right_pair[1])
                 self.bytepair_counting_dict[new_right_pair] += source_word_count
-                # 修改右边pair的倒查索引
-                right_pair_source_word_index_list = self.bytepair_from_words_index[right_pair]
-                target_idx = 0
-                while target_idx < len(right_pair_source_word_index_list):
-                    if right_pair_source_word_index_list[target_idx][0] == i and right_pair_source_word_index_list[target_idx][1] == j-1:
-                        break
-                    target_idx += 1
-                right_pair_source_word_index_list.pop(target_idx)
-                self.bytepair_from_words_index[right_pair] = right_pair_source_word_index_list
-
-            # 更新word_tokenizing
-            if j >= 1:
-                word_tokenizing[j-1] = new_left_pair
-                self.bytepair_from_words_index[new_left_pair].append((i,j-1))
-            if j <= len(word_tokenizing)-2:
+                # 反查index的列表，新增new pair
+                self.bytepair_from_words_index[new_right_pair].append(index)
                 word_tokenizing[j+1] = new_right_pair
-                self.bytepair_from_words_index[new_right_pair].append((i,j))
+                # 考虑一种情况，如果right_pair == middle pair的时候，会出现索引到同一张表，所以要从右边一位查起
+                # 删去旧的
+                del_idx = 0
+                if right_pair == most_frequent_pair:
+                    print("触发midle和right pair一样的情况: ", word_tokenizing)
+                    del_idx = pos+1
+                while del_idx < len(self.bytepair_from_words_index[right_pair]):
+                    if self.bytepair_from_words_index[right_pair][del_idx] == index:
+                        break
+                    del_idx += 1
+                self.bytepair_from_words_index[right_pair].pop(del_idx)
+
             word_tokenizing.pop(j)
-            self.words_tokenizing_states[i] = word_tokenizing
+            self.words_tokenizing_states[index] = word_tokenizing
 
         # 删除被合并的词组的倒查索引列表
         del self.bytepair_from_words_index[most_frequent_pair]
+        # 删除字节对
+        del self.bytepair_counting_dict[most_frequent_pair]
 
         # 更新词表
         self.BPE_TOKEN_TABLE[len(self.BPE_TOKEN_TABLE)] = new_token
@@ -140,7 +149,7 @@ class BPETrainer:
         for pre_tokenized_text in self.pre_tokenized_text_list:
             for word in pre_tokenized_text:
                 self.words_counting[word] += 1
-        print("#########【测试点位-2】：词统计\n", self.words_counting, "\n\n\n")
+        # print("#########【测试点位-2】：词统计\n", self.words_counting, "\n\n\n")
         
         # 2025.12.28 写完了初始化
         # 维护一个word列表，使得byte-pair可以反查出自哪个index
@@ -148,8 +157,7 @@ class BPETrainer:
         self.words_tokenizing_states = []
         for word, counts in self.words_counting.items():
             # 对每个word进行encoding -> 其实是基于已有的词表进行分词
-            word_encoding = word.encode("utf-8")
-            word_split = [char_byte for char_byte in word_encoding]
+            word_split = [ch.encode("utf-8") for ch in word]
             if len(word_split) <= 1: # 只有一个字母的没办法构成byte-pair
                 continue
             word_tokenizing = []
@@ -157,35 +165,38 @@ class BPETrainer:
                 byte_pair = (word_split[i], word_split[i+1])
                 word_tokenizing.append(byte_pair)
                 self.bytepair_counting_dict[byte_pair] += counts
-                self.bytepair_from_words_index[byte_pair].append((len(self.words_tokenizing_states),len(word_tokenizing)-1))
+                self.bytepair_from_words_index[byte_pair].append(len(self.words_tokenizing_states))
             self.words_tokenizing_states.append(word_tokenizing)
-        print("#########【测试点位-3】：分词状态列表\n", self.words_tokenizing_states, "\n\n\n")
-        print("#########【测试点位-4】：bytepair倒查索引列表\n", self.bytepair_from_words_index, "\n\n\n")
-        aaa
-
+        # print("#########【测试点位-3】：分词状态列表\n", self.words_tokenizing_states, "\n\n\n")
+        # print("#########【测试点位-4】：bytepair倒查索引列表\n", self.bytepair_from_words_index, "\n\n\n")
         
         # 2025.12.29 基于已经初始化的内容
         # 反复merge
         TABLE_SIZE = len(self.BPE_TOKEN_TABLE)
         idx = 256
         iter_time = 0
+        print(f"##### 初始化的bytepair统计表：{self.bytepair_counting_dict}\n\n\n")
         while TABLE_SIZE <= self.vocab_size:
             iter_time += 1
             # 找到统计次数最多的词表
             # 找出所有counts最多的byte pairs
             max_count = max(self.bytepair_counting_dict.values())
             most_frequent_pairs = [pair for pair, count in self.bytepair_counting_dict.items() if count == max_count]
-            print(f"Iter-{iter_time}, most frequent pairs: ({bytes([most_frequent_pairs[0]])},{bytes([most_frequent_pairs[1]])}), count: {max_count}")
-            
             ## 根据字典序找出其中一个pair，max天然就是字典序找最大
             # 先比较第一个元素，在比较第二个
             most_frequent_pair = max(most_frequent_pairs)
+            print(f"Iter-{iter_time} 当前最频繁pair: ", most_frequent_pair, max_count)
+            # print(f"#########【测试点位-5】：最频繁的bytepair\nIter-{iter_time}, most frequent pairs: ({most_frequent_pair}), count: {max_count}\n\n")
             self.merge_and_update_bytepair(most_frequent_pair)
+            print(f"##### Iter-{iter_time}：\n最频繁的pair是：{most_frequent_pair}\n\n\n")
             TABLE_SIZE += 1
-
+        print("完成后的词表：\n", self.BPE_TOKEN_TABLE)
 
 if __name__ == "__main__":
-    # obj = BPETrainer(input_path="datasets/TinyStoriesV2-GPT4-valid.txt", vocab_size=1000, special_tokens=["<|endoftext|>"])
+    start_time = time.time()
+    vocab_size = 1000
+    obj = BPETrainer(input_path="datasets/TinyStoriesV2-GPT4-valid.txt", vocab_size=vocab_size, special_tokens=["<|endoftext|>"])
+    print(f"目标词表大小：{vocab_size}, 耗时{time.time() - start_time}")
 
     ## 测试样例
-    test_obj = BPETrainer(input_path="datasets/test-sentences.txt", vocab_size=260, special_tokens=["<|endoftext|>"])
+    # test_obj = BPETrainer(input_path="datasets/test-sentences.txt", vocab_size=260, special_tokens=["<|endoftext|>"])
